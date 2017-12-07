ba5b26c75aed3b403e72b5259cc9b133624667db
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2017-12-07 17:08:36 -0600

    Setup basic home page to return minimal html

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,8 @@
 """
 Lists views
 """
+from django.http import HttpResponse
 
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do Lists</title></html>')
