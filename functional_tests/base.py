520768e9164c839b8b341fe918d3a5198d0ab2c2
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2017-12-07 19:34:51 -0600

    Refactor functional test's row text lookup to stop replication

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -23,6 +23,11 @@ class NewVisitorTest(unittest.TestCase):
         Clean up after tests.
         """
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Emily has heard about a cool new online to-do app. She goes to check out its homepage
@@ -44,10 +49,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy cat toys" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy cat toys', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy cat toys')
         
         # There is still a text box inviting her to add another item. She enters "Surprise cats with toys"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -55,10 +57,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy cat toys', [row.text for row in rows])
-        self.assertIn('2: Surprise cats with toys', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy cat toys')
+        self.check_for_row_in_list_table('2: Surprise cats with toys')
 
         # The page updates again, and now shows both items on her list
 
