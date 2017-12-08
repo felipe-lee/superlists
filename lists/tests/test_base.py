a965b7c8cf532662c6c18519f368451d6012d202
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2017-12-07 20:18:14 -0600

    Setup view to save items and redirect after post, show items in template

--- lists/tests/test_base.py
+++ lists/tests/test_base.py
@@ -5,6 +5,7 @@ Base tests for lists app
 from django.test import TestCase
 from django.urls import resolve
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -21,8 +22,32 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         list_item_text = 'A new list item'
+
+        self.client.post('/', data={'item_text': list_item_text})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, list_item_text)
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
     
-        response = self.client.post('/', data={'item_text': list_item_text})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
     
-        self.assertIn(list_item_text, response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        text_1 = 'itemy 1'
+        text_2 = 'itemy 2'
+    
+        Item.objects.create(text=text_1)
+        Item.objects.create(text=text_2)
+    
+        response = self.client.get('/')
+    
+        self.assertIn(text_1, response.content.decode())
+        self.assertIn(text_2, response.content.decode())
