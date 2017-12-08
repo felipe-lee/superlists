b98fdb52f57106303642ecb2bf1e10ab3f5ab59a
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2017-12-07 22:17:11 -0600

    Add functional test for handling multiple users
    Also pull out some constants to make writing the tests easier

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -10,6 +10,18 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
 MAX_TIME = 10
+E_ITEM_1 = 'Buy cat toys'
+E_ITEM_2 = 'Surprise cats with toys'
+F_ITEM_1 = 'Look for deals on computer parts'
+F_ITEM_2 = 'Build a new computer'
+
+
+def get_webdriver():
+    """
+    Setup a webdriver
+    """
+    return webdriver.Chrome()
+    # return webdriver.Firefox()
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -63,7 +75,7 @@ class NewVisitorTest(LiveServerTestCase):
             else:
                 break
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Emily has heard about a cool new online to-do app. She goes to check out its homepage
         self.browser.get(self.live_server_url)
         
@@ -90,15 +102,59 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Surprise cats with toys')
         inputbox.send_keys(Keys.ENTER)
 
+        # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy cat toys')
         self.wait_for_row_in_list_table('2: Surprise cats with toys')
 
-        # The page updates again, and now shows both items on her list
-
-        # Emily wonders whether the site will remember her list. Then she sees that teh site has generated a unique url
-        # for her -- there is some explanatory text to that effect.
-        self.fail('Finish the test!')
-        
-        # She visits that URL - her to-do list is still there.
-        
         # Satisfied, she goes to sleep.
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Emily starts a new to-do list
+        self.browser.get(self.live_server_url)
+    
+        inputbox = self.browser.find_element_by_id('id_new_item')
+    
+        inputbox.send_keys(E_ITEM_1)
+        inputbox.send_keys(Keys.ENTER)
+    
+        self.wait_for_row_in_list_table(f'1: {E_ITEM_1}')
+    
+        # She notices that her list has a unique URL.
+        emily_list_url = self.browser.current_url
+        self.assertRegex(emily_list_url, '/lists/.+')
+    
+        # Now a new user, Felipe, comes along to teh site.
+    
+        # # We use a new browser session to make sure that no information of Emily's is coming through from cookies,
+        self.browser.quit()
+    
+        self.browser = get_webdriver()
+    
+        # Felipe visits the home page. There is no sign of Emily's list
+        self.browser.get(self.live_server_url)
+    
+        page_text = self.browser.find_element_by_tag_name('body').text
+    
+        self.assertNotIn(E_ITEM_1, page_text)
+        self.assertNotIn(E_ITEM_2, page_text)
+    
+        # Felipe starts a new list by entering a new item. He is into techy stuff...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+    
+        inputbox.send_keys(F_ITEM_1)
+        inputbox.send_keys(Keys.ENTER)
+    
+        self.wait_for_row_in_list_table(f'1: {F_ITEM_1}')
+    
+        # Felipe gets his own unique URL
+        felipe_list_url = self.browser.current_url
+        self.assertRegex(felipe_list_url, '/lists/.+')
+        self.assertNotEqual(felipe_list_url, emily_list_url)
+    
+        # Again, there is no trace of Emily's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+    
+        self.assertNotIn(E_ITEM_1, page_text)
+        self.assertNotIn(E_ITEM_2, page_text)
+    
+        # Satisfied, they both go back to sleep
