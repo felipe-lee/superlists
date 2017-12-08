9b507caf82493bb59f735990fdf57bfc2063ed2c
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2017-12-07 18:12:51 -0600

    Setup some basic html to get closer to the functional tests

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -51,7 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy cat toys' for row in rows)
+            any(row.text == '1: Buy cat toys' for row in rows),
+            "New to-do item did not appear in table"
         )
         # There is still a text box inviting her to add another item. She enters "Surprise cats with toys"
         self.fail('Finish the test!')
