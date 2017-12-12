f8e701582e35a4f603a100acc7c9269bbd6530de
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2017-12-11 23:11:05 -0600

    Use get_absolute_url on List model to DRY urls in views

--- lists/models.py
+++ lists/models.py
@@ -3,10 +3,20 @@
 Lists models
 """
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class List(models.Model):
-    pass
+    """
+    Model to keep track of lists
+    """
+    
+    def get_absolute_url(self):
+        """
+        Retrieve url to view list in live website
+        :return: list url in live website
+        """
+        return reverse_lazy('lists:view_list', kwargs={'list_id': self.id})
 
 
 class Item(models.Model):
--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,6 @@ Lists views
 """
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
 
 from lists.models import Item, List
 
@@ -33,7 +32,7 @@ def new_list(request):
     else:
         item.save()
 
-    return redirect(reverse_lazy('lists:view_list', kwargs={'list_id': list_.id}))
+    return redirect(list_)
 
 
 def view_list(request, list_id):
@@ -53,7 +52,7 @@ def view_list(request, list_id):
             context['error'] = "You can't have an empty list item"
         else:
             item.save()
-        
-            return redirect(reverse_lazy('lists:view_list', kwargs={'list_id': list_.id}))
+
+            return redirect(list_)
 
     return render(request, 'lists/list.html', context)
