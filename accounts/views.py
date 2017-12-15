0743b5ed765b90344249e5b6ebca9d435040d168
Felipe Lee <felipe.lee.garcia@gmail.com>
2017-12-15 16:59:21 -0600

    Add some more structure for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,11 @@
 """
 Views for accounts app
 """
+import sys
 import uuid
+
+from django.core.mail import send_mail
+from django.shortcuts import render
 
 from accounts.models import Token
 
@@ -17,4 +21,15 @@ def send_login_email(request):
 
     Token.objects.create(email=email, uid=uid)
 
-    print(f'Saving uid ({uid}) for email: {email}. | {')
+    print(f'Saving uid ({uid}) for email: {email}. | {file=sys.stderr}')
+
+    url = request.build_absolute_uri(f'/accounts/login?uid={uid}')
+
+    send_mail(
+        'Your loign link for Superslists',
+        f'Use this link to log in:\n\n{url}',
+        'noreply@superlists',
+        [email]
+    )
+
+    return render(request, 'accounts/login_email_sent.html')
