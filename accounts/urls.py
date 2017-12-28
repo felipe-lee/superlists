7e162b2e44627979d084238bd83c7bbf8b277048
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2017-12-28 16:02:08 -0600

    Add logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@
 """
 Accounts app URL Configuration
 """
-from django.urls import path
+from django.contrib.auth.views import logout
+from django.urls import path, reverse_lazy
 
 from accounts.views import login, send_login_email
 
@@ -10,4 +11,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('send-login-email', send_login_email, name='send_login_email'),
     path('login', login, name='login'),
+    path('logout', logout, {'next_page': reverse_lazy('home')}, name='logout')
 ]
