f85e4a5a6aed49f6b225a8aeab6470635ca5e64b
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2018-01-12 00:23:30 -0600

    Fix fab run settings

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -3,21 +3,39 @@
 Tools to run on server
 """
 from fabric.api import run
+from fabric.context_managers import settings
 
 
 def _get_manage_dot_py(host):
+    """
+    Retrieves a string containing the paths to the python interpreter and the manage.py file for the given host
+    :param host: host to find the manage.py file for
+    :return: string with paths
+    """
     return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/source/manage.py'
 
 
 def reset_database(host):
+    """
+    Flush remote db
+    :param host: site name
+    """
     manage_dot_py = _get_manage_dot_py(host)
     
-    run(f'{manage_dot_py} flush --noinput')
+    with settings(use_ssh_config=True, hosts=['google-superlists-elspeth']):
+        run(f'{manage_dot_py} flush --noinput')
 
 
 def create_session_on_server(host, email):
+    """
+    Creates a session on server
+    :param host: site name
+    :param email: email to create a session for
+    :return: session key
+    """
     manage_dot_py = _get_manage_dot_py(host)
     
-    session_key = run(f'{manage_dot_py} create_session {email}')
-    
-    return session_key.strip()
+    with settings(use_ssh_config=True, hosts=['google-superlists-elspeth']):
+        session_key = run(f'{manage_dot_py} create_session {email}')
+        
+        return session_key.strip()
