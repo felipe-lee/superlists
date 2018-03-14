6cfb652ce7629477698e3b57cfa2db9e6fbbae7c
Felipe Lee <felipe.lee.garcia@gmail.com>
2018-03-13 20:21:17 -0500

    Add login/logout related methods to ListPage

--- functional_tests/list_page.py
+++ functional_tests/list_page.py
@@ -72,6 +72,80 @@ class ListPage(object):
 
         return self
 
+    @wait
+    def wait_to_be_logged_in(self, email):
+        """
+        Waits for user to be logged in.
+        :param email: user email
+        """
+        self.test.browser.find_element_by_link_text('Log out')
+
+        navbar = self.test.browser.find_element_by_css_selector('.navbar')
+
+        self.test.assertIn(email, navbar.text)
+
+    def get_email_input(self):
+        """
+        Retrieves the email input field from the dom
+        :return: email input element
+        """
+        return self.test.browser.find_element_by_name('email')
+
+    def enter_email(self, email):
+        """
+        Enters email in email input
+        :param email: email to log in with
+        :return: self
+        """
+        email_input = self.get_email_input()
+
+        email_input.send_keys(email)
+        email_input.send_keys(Keys.ENTER)
+
+        return self
+
+    def log_in(self, email):
+        """
+        Logs user in using email
+        :param email: email to log in with
+        :return: self
+        """
+        self.enter_email(email)
+
+        self.wait_to_be_logged_in(email)
+
+        return self
+
+    @wait
+    def wait_to_be_logged_out(self, email):
+        """
+        Wait for user to be logged out.
+        :param email: user email
+        """
+        self.get_email_input()
+
+        navbar = self.test.browser.find_element_by_css_selector('.navbar')
+
+        self.test.assertNotIn(email, navbar.text)
+
+    def click_log_out_link(self):
+        """
+        Clicks log out link
+        :return: self
+        """
+        self.test.browser.find_element_by_link_text('Log out').click()
+
+    def log_out(self):
+        """
+        Logs user out
+        :return: self
+        """
+        self.click_log_out_link()
+
+        self.wait_to_be_logged_out()
+
+        return self
+
     def get_share_box(self):
         """
         Retrieves the share box from the dom
