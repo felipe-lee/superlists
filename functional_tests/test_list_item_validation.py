0f1662cd190ee2041ebbe55a5e374d0ee5ced1e2
Felipe Lee <felipe.lee.garcia@gmail.com>
2018-03-15 00:35:02 -0500

    Refactor ItemValidationTest to use ListPage

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,6 +5,7 @@ List item validation FTs
 from selenium.webdriver.common.keys import Keys
 
 from functional_tests.base import FunctionalTest
+from functional_tests.list_page import ListPage
 
 E_ITEM_1 = 'Buy salmon'
 E_ITEM_2 = 'Make salmon dinner'
@@ -23,36 +24,39 @@ class ItemValidationTest(FunctionalTest):
         # Emily goes to the home page and accidentally tries to submit an empty list item. She hits enter on the empty
         # input box.
         self.browser.get(self.live_server_url)
-        self.enter_input('')
+
+        list_page = ListPage(self)
+
+        list_page.enter_list_item('')
 
         # The browser intercepts the request, and does not load the list page
         self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:invalid'))
 
         # She starts typing some text for the new item and the error disappears
-        self.set_inputbox()
-        self.inputbox.send_keys(E_ITEM_1)
+        input_box = list_page.get_item_input_box()
+        input_box.send_keys(E_ITEM_1)
 
         self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:valid'))
 
         # And she can submit it successfully
-        self.inputbox.send_keys(Keys.ENTER)
+        input_box.send_keys(Keys.ENTER)
         
         self.wait_for_row_in_list_table(f'1: {E_ITEM_1}')
         
         # Perversely, she now decides to submit a second blank list item
-        self.enter_input('')
+        list_page.enter_list_item('')
 
         # Again the browser will not comply
         self.wait_for_row_in_list_table(f'1: {E_ITEM_1}')
         self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:invalid'))
 
         # And she can correct it filling some text in
-        self.set_inputbox()
-        self.inputbox.send_keys(E_ITEM_2)
+        input_box = list_page.get_item_input_box()
+        input_box.send_keys(E_ITEM_2)
 
         self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:valid'))
 
-        self.inputbox.send_keys(Keys.ENTER)
+        input_box.send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table(f'1: {E_ITEM_1}')
         self.wait_for_row_in_list_table(f'2: {E_ITEM_2}')
@@ -61,10 +65,10 @@ class ItemValidationTest(FunctionalTest):
         # Emily goes to the new home page and starts a new list
         self.browser.get(self.live_server_url)
 
-        self.add_list_item(E_ITEM_1)
-    
+        list_page = ListPage(self).add_list_item(E_ITEM_1)
+
         # She accidentally tries to enter a duplicate item
-        self.enter_input(E_ITEM_1)
+        list_page.enter_list_item(E_ITEM_1)
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
@@ -76,17 +80,17 @@ class ItemValidationTest(FunctionalTest):
         # Emily starts a list and causes a validation error:
         self.browser.get(self.live_server_url)
 
-        self.add_list_item(E_ITEM_1)
+        list_page = ListPage(self).add_list_item(E_ITEM_1)
 
-        self.enter_input(E_ITEM_1)
+        list_page.enter_list_item(E_ITEM_1)
 
         self.wait_for(lambda: self.assertTrue(
             self.get_error_element().is_displayed()
         ))
 
         # She starts typing in the input box to clear the error
-        self.set_inputbox()
-        self.inputbox.send_keys(E_ITEM_2)
+        input_box = list_page.get_item_input_box()
+        input_box.send_keys(E_ITEM_2)
 
         # She is pleased to see that the error message disappears
         self.wait_for(lambda: self.assertFalse(
