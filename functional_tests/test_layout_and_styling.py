95806addadac47bc1e5453f0eac457a3f611d323
Felipe Lee <felipe.lee.garcia@gmail.com>
2018-03-15 00:34:23 -0500

    Refactor LayoutAndStylingTest to use ListPage

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -4,6 +4,7 @@ Layout and styling functional tests
 """
 
 from functional_tests.base import FunctionalTest
+from functional_tests.list_page import ListPage
 
 
 class LayoutAndStylingTest(FunctionalTest):
@@ -12,11 +13,14 @@ class LayoutAndStylingTest(FunctionalTest):
         # Emily goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-        
+
+        list_page = ListPage(self)
+
         # She notices the input box is nicely centered
-        self.set_inputbox()
+        input_box = list_page.get_item_input_box()
+
         self.assertAlmostEqual(
-            self.inputbox.location['x'] + self.inputbox.size['width'] / 2,
+            input_box.location['x'] + input_box.size['width'] / 2,
             512,
             delta=10
         )
@@ -24,12 +28,12 @@ class LayoutAndStylingTest(FunctionalTest):
         # She starts a new list and sees the input is nicely centered there too
         new_text = 'random item'
 
-        self.add_list_item(new_text)
+        list_page.add_list_item(new_text)
 
-        self.set_inputbox()
+        input_box = list_page.get_item_input_box()
         
         self.assertAlmostEqual(
-            self.inputbox.location['x'] + self.inputbox.size['width'] / 2,
+            input_box.location['x'] + input_box.size['width'] / 2,
             512,
             delta=10
         )
